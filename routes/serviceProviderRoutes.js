4adc818756c036821231ddeafcd1e4ddb8821c47
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2024-09-12 16:35:47 +0530

    added service provider controller and routes

---
+++ routes/serviceProviderRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
+const UnverifiedEmailSP = require('../models/unverifiedEmailSP');
+const bcrypt = require('bcrypt');
+const multer = require('multer');
+const Order = require('../models/order');
+const cloudinary = require('cloudinary').v2;
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const { transporter } = require('../utils/email');
+const Customer = require('../models/customer');
+const { serviceProviderLogin, serviceProviderRegister, serviceProviderVerify, serviceProviderVerifyToken } = require('../controllers/serviceProviderControllers');
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+//body params: uuid, name, email
+
+/* response:
+  message: String
+*/
+
+router.post('/registerSP', serviceProviderRegister);
+
+router.post('/verifySP', serviceProviderVerify);
+
+router.post('/loginSP', serviceProviderLogin);
+
+router.post('/verifyTokenSP', serviceProviderVerifyToken);
+
+module.exports = router;
